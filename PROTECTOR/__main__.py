92e17354bbc81b9724d58ebbfc14df6d27eaf66b
Sumankumar81547 <168259000+Sumankumar81547@users.noreply.github.com>
2024-05-08 16:48:03 +0530

    Update __main__.py

--- PROTECTOR/__main__.py
+++ PROTECTOR/__main__.py
@@ -4,7 +4,7 @@ from pyrogram import idle
 from PROTECTOR import PROTECTOR
 from PROTECTOR.modules import ALL_MODULES
 
-LOGGER_ID = -1002059718978
+LOGGER_ID = -1002021288289
 
 loop = asyncio.get_event_loop()
 
@@ -14,7 +14,7 @@ async def JARVIS():
     print("Bot Started Successfully")
     await idle()
     print("MAI HU PIRO CODER BOLO NHI AAYA ERROR")
-    await PROTECTOR.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [JARVIS](https://t.me/JARVIS_V2)**")
+    await PROTECTOR.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [MOHIT](https://t.me/Mohitkumar076)**")
 
 if __name__ == "__main__":
     loop.run_until_complete(JARVIS())
